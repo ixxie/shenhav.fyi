d6ed206718a7f95e51761e4b479ef79150c7e5ae
Matan Bendix Shenhav <matan@shenhav.fyi>
2023-03-20 22:05:03 +0100

    refactor: move footer to own page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,8 @@
 	import { color } from './stores';
 	import { pages } from './pages';
 
+	import Footer from './Footer.svelte';
+
 	// page data
 	const pageNames = Object.keys(pages);
 	$: route = $page.route.id ?? '/';
@@ -48,31 +50,7 @@
 			</div>
 			<slot />
 		</main>
-		<footer>
-			<p>
-				<span>
-				Copyright © 2023
-				</span>
-				<span class="dash"/>
-				<span>
-				Matan Bendix Shenhav
-				</span>
-				<span class="dash"/>
-				<span>
-				All Rights Reserved
-				</span>
-			</p>
-			<p>
-				<span><i>3D Illustration credit</i></span>
-				<span class="dash"/>
-				<span><a href={currentPage.modelUrl}>{currentPage.model}</a> by <a href={currentPage.authorUrl}>{currentPage.author}</a></span>
-			</p>
-			<p style="font-size: smaller; max-width: 500px;">
-				<span><i>Modifications</i></span>
-				<span class="dash"/>
-				<span>{currentPage.notes}</span>
-			</p>
-		</footer>
+		<Footer {currentPage}/>
 	{/if}
 </div>
 <div id="background" {style} />
@@ -127,26 +105,6 @@
 		z-index: -99;
 	}
 
-	footer {
-		padding: 3rem 0;
-		display: flex;
-		flex-flow: column;
-		align-items: center;
-		text-align: center;
-		gap: 1rem;
-		opacity: 0.5;
-	}
-
-	footer p {
-		display: flex;
-		flex-flow: row wrap;
-		justify-content: center;
-		gap: 0.5em;
-	}
-
-	.dash::before {
-		content: '-';
-	}
 
 	@media (max-width: 1800px) {
 		:global(body) {
@@ -168,14 +126,6 @@
 		#content {
 			width: 100%;
 			max-width: 100%;
-		}
-
-		footer p {
-			flex-flow: column;
-		}
-
-		footer p .dash {
-			display: none;
 		}
 	}
 
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	import type { Page } from "./pages";
+
+    export let currentPage: Page;
+</script>
+
+<footer>
+	<p>
+		<span>
+		    Copyright © 2023
+		</span>
+		<span class="dash"/>
+		<span>
+		    Matan Bendix Shenhav
+		</span>
+		<span class="dash"/>
+		<span>
+		    All Rights Reserved
+		</span>
+	</p>
+	<p>
+		<span><i>3D Illustration credit</i></span>
+		<span class="dash"/>
+		<span><a href={currentPage.modelUrl}>{currentPage.model}</a> by <a href={currentPage.authorUrl}>{currentPage.author}</a></span>
+	</p>
+	<p style="font-size: smaller; max-width: 500px;">
+		<span><i>Modifications</i></span>
+		<span class="dash"/>
+		<span>{currentPage.notes}</span>
+	</p>
+</footer>
+
+<style>
+	footer {
+		padding: 3rem 0;
+		display: flex;
+		flex-flow: column;
+		align-items: center;
+		text-align: center;
+		gap: 1rem;
+		opacity: 0.5;
+	}
+
+	footer p {
+		display: flex;
+		flex-flow: row wrap;
+		justify-content: center;
+		gap: 0.5em;
+	}
+
+	.dash::before {
+		content: '-';
+	}
+
+	@media (max-width: 1100px) {
+		footer p {
+			flex-flow: column;
+		}
+
+		footer p .dash {
+			display: none;
+		}
+	}
+</style>
